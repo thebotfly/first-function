b7e65e0a703d0e2714b43e27476bc4b542af4f1b
Luke plaisance <lukeplaisance2002@outlook.com>
2020-08-31 16:18:42 -0500

    started on it

--- Game.cs
+++ Game.cs
@@ -5,23 +5,32 @@ using System.Text;
 namespace HelloWorld
 {
     class Game
+   
     {
+        bool gameover = false;
         //Run the game
         public void Run()
         {
+            Start();
             
+            while(gameover == false)
+            {
+                Update();
+            }
+           
         }
 
         //Performed once when the game begins
         public void Start()
         {
-            
+            Console.WriteLine("Welcome to my game.");  
         }
 
         //Repeated until the game ends
         public void Update()
         {
-            
+            Console.WriteLine("Test Update");
+            Console.ReadKey();
         }
 
         //Performed once when the game ends
